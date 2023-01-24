89f605980ddace00d09fd92a39c8e2e1a16a9cfe
FawazHutomiA <hutomifawaza@gmail.com>
2023-01-24 10:21:03 +0700

    add: tailwind config

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/components/**/*.vue",
+    "./src/layouts/**/*.vue",
+    "./src/fragments/**/*.vue",
+    "./src/views/**/*.vue",
+    "./index.html",
+    "./src/core/*.js"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Rubik", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
