4b14b4435b3f4f77b457fd05ae9a8090e52cd194
FawazHutomiA <hutomifawaza@gmail.com>
2023-01-24 11:36:41 +0700

    updated: navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,13 @@
 <template>
-    <div>navbar</div>
+    <div class="bg-white flex px-32 min-h-[3.25rem] place-items-center my-10 justify-between">
+        <img src="/icon.png" class="w-14" alt="">
+        <div class="flex gap-x-6 text-lg">
+            <div class="py-1 cursor-pointer hover:text-violet-500 transition duration-300 ease-in-out">Portofolio</div>
+            <div class="border-2 border-violet-500 text-violet-700 hover:bg-violet-500 hover:text-white transition duration-300 ease-in-out rounded-md px-3 py-1 cursor-pointer">
+                <span>Say Hello</span>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
 export default {
