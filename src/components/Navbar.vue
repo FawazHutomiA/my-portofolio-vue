d69cb15706b3373d180bd937c5207e00d1549590
FawazHutomiA <hutomifawaza@gmail.com>
2023-01-24 13:53:53 +0700

    changed: icon style

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-white flex px-32 min-h-[3.25rem] place-items-center my-10 justify-between">
-        <img src="/icon.png" class="w-14" alt="">
+        <img src="/icon.png" class="w-24 cursor-pointer hover:border-2 hover:rounded-full hover:border-violet-500 hover:transition hover:duration-300 hover:ease-in-out p-2" alt="">
         <div class="flex gap-x-6 text-lg">
             <div class="py-1 cursor-pointer hover:text-violet-500 transition duration-300 ease-in-out">Portofolio</div>
             <div class="border-2 border-violet-500 text-violet-700 hover:bg-violet-500 hover:text-white transition duration-300 ease-in-out rounded-md px-3 py-1 cursor-pointer">
