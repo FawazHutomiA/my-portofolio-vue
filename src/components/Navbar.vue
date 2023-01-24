d84401e88771c206585cbc23022081dcbe2ff151
FawazHutomiA <hutomifawaza@gmail.com>
2023-01-24 17:00:05 +0700

    updated: navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-white flex px-32 min-h-[3.25rem] place-items-center my-10 justify-between">
+    <div class="bg-white flex px-32 min-h-[3.25rem] place-items-center my-6 justify-between">
         <img src="/icon.png" class="w-24 cursor-pointer hover:border-2 hover:rounded-full hover:border-violet-500 hover:transition hover:duration-300 hover:ease-in-out p-2" alt="">
         <div class="flex gap-x-6 text-lg">
             <div class="py-1 cursor-pointer hover:text-violet-500 transition duration-300 ease-in-out">Portofolio</div>
