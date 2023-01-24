d26d0e301594aa84b318a50e3781c29d89ab0be6
FawazHutomiA <hutomifawaza@gmail.com>
2023-01-24 11:37:08 +0700

    add: me fragment

---
+++ src/fragments/home/Me.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="mt-24 flex flex-col items-center gap-y-4">
+        <h1 class="text-5xl text-[#141c3a] font-semibold">Frontend Developer & Backend Developer</h1>
+        <p class="text-[#141c3a] text-xl">I design and code beautifully simple things, and I love what I do.</p>
+    </div>
+</template>
+<script>
+export default{
+    name: 'Me'
+}
+</script>
