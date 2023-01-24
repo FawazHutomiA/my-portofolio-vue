e41f9029d20c40a69e99206aa4dab4f37f679417
FawazHutomiA <hutomifawaza@gmail.com>
2023-01-24 13:54:09 +0700

    updated: me

--- src/fragments/home/Me.vue
+++ src/fragments/home/Me.vue
@@ -1,7 +1,13 @@
 <template>
-    <div class="mt-24 flex flex-col items-center gap-y-4">
-        <h1 class="text-5xl text-[#141c3a] font-semibold">Frontend Developer & Backend Developer</h1>
-        <p class="text-[#141c3a] text-xl">I design and code beautifully simple things, and I love what I do.</p>
+    <div class="mt-24 flex flex-col items-center gap-y-14">
+        <div class="flex flex-col gap-y-4 text-center">
+            <h1 class="text-5xl text-[#141c3a] font-semibold">Frontend Developer & Backend Developer</h1>
+            <p class="text-[#141c3a] text-xl">I design and code beautifully simple things, and I love what I do.</p>
+        </div>
+        <div class="lg:grid lg:grid-cols-2">
+            <img src="/avatar.png" alt="" class="w-56">
+            <img src="/hero-devices.gif" alt="" class="-mt-24">
+        </div>
     </div>
 </template>
 <script>
