935a302dbb35f126a2d754a57617c57e1aeea2dd
FawazHutomiA <hutomifawaza@gmail.com>
2023-01-24 17:01:05 +0700

    updated: me

--- src/fragments/home/Me.vue
+++ src/fragments/home/Me.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="mt-24 flex flex-col items-center gap-y-14">
+    <div class="flex flex-col items-center gap-y-14">
         <div class="flex flex-col gap-y-4 text-center">
-            <h1 class="text-5xl text-[#141c3a] font-semibold">Frontend Developer & Backend Developer</h1>
+            <h1 class="text-5xl text-[#141c3a] font-semibold">Design, Frontend Developer & Mentor</h1>
             <p class="text-[#141c3a] text-xl">I design and code beautifully simple things, and I love what I do.</p>
         </div>
         <div class="lg:grid lg:grid-cols-2">
