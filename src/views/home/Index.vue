cad27004803412bd5a154b418a96b24a4747bb31
FawazHutomiA <hutomifawaza@gmail.com>
2023-01-24 17:01:49 +0700

    updated: view home

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="container mx-auto">
-    <me></me>
+  <div class="mx-auto">
+    <me class="mt-20"></me>
+    <about class="mt-20"></about>
   </div>
 </template>
 <script>
 import Me from "@/fragments/home/Me.vue";
+import About from "@/fragments/home/About.vue";
 export default {
   name: "Home",
   components: {
     Me,
+    About,
   },
   data() {
     return {};
